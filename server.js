e8f4a1b32a29418d19ad13e6e17918d5bc3cb61a
Ricardo Casares <ricardocasares@gmail.com>
2017-12-03 05:24:06 +0100

    Handle promise

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const app = next({ dev: process.env.NODE_ENV !== "production" });
 const handler = routes.getRequestHandler(app);
 const { createServer } = require("http");
 
-app.prepare().then(() => {
-  createServer(handler).listen(3000);
-});
+app
+  .prepare()
+  .then(() => {
+    createServer(handler).listen(3000);
+  })
+  .catch(console.error);
