54a10169184ab6de42ed41432a3106dfd3e3f9a8
Ricardo Casares <ricardocasares@gmail.com>
2018-12-31 17:49:58 +0100

    update posts

--- pages/changelog.tsx
+++ pages/changelog.tsx
@@ -3,8 +3,15 @@ import Box from "@app/components/Box";
 import Page from "@app/components/Page";
 import { H1, H2, Lead, Strong } from "@app/components/Typography";
 import Link from "@app/components/Link";
+import { request } from "@app/lib/github";
 
-export const Changelog: FunctionComponent = () => (
+export type Post = {
+  number: number;
+  title: string;
+  createdAt: Date;
+};
+
+export const Changelog: FunctionComponent<{ posts: Post[] }> = ({ posts }) => (
   <Page>
     <Box display="flex" flex="1">
       <main>
@@ -13,25 +20,10 @@ export const Changelog: FunctionComponent = () => (
           Notes and thoughts on <Strong>software development</Strong>.
         </Lead>
         $
-        {[
-          "Scrum cheat sheet",
-          "Mocking is a code smell",
-          "What is recursion anyway",
-          "Function composition",
-          "About transducers",
-          "Kanban cheat sheet",
-          "How the web works",
-          "Scrum cheat sheet",
-          "Mocking is a code smell",
-          "What is recursion anyway",
-          "Function composition",
-          "About transducers",
-          "Kanban cheat sheet",
-          "How the web works"
-        ].map(post => (
-          <H2>
-            <Link href="/">
-              <a>{post}</a>
+        {posts.map(post => (
+          <H2 key={post.number}>
+            <Link href={`/changelog/${post.number}/post`}>
+              <a>{post.title}</a>
             </Link>
           </H2>
         ))}
@@ -39,5 +31,36 @@ export const Changelog: FunctionComponent = () => (
     </Box>
   </Page>
 );
+
+const QUERY = `query($owner: String!, $name: String!) {
+  repository(owner: $owner, name: $name) {
+    issues(last: 20, states: OPEN) {
+      nodes {
+        number,
+        title,
+        createdAt,
+        comments {
+          totalCount
+        }
+        labels(first: 5) {
+          nodes {
+            name,
+            color
+          }
+        }
+      }
+    }
+  }
+}`;
+
+// @ts-ignore
+Changelog.getInitialProps = async () => {
+  const data = await request<{ repository: { issues: { nodes: Post[] } } }>(
+    QUERY,
+    { owner: "ricardocasares", name: "analogical" }
+  );
+
+  return { posts: data.repository.issues.nodes };
+};
 
 export default Changelog;
