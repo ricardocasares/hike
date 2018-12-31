11b9663485f1a65d9f5d27b71af131b04e7df989
Ricardo Casares <ricardocasares@gmail.com>
2018-12-31 03:00:47 +0100

    add meta theme color

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -17,6 +17,7 @@ export default class MyApp extends App {
             name="viewport"
             content="width=device-width, initial-scale=1.0"
           />
+          <meta name="theme-color" content="#000000" />
         </Head>
 
         <Global styles={css} />
