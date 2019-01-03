635738f3883952ad5d1a836e04ebf3a594cbea58
Ricardo Casares <ricardocasares@gmail.com>
2019-01-03 22:25:25 +0100

    add bundlesize checks

--- next.config.js
+++ next.config.js
@@ -1,5 +1,3 @@
-const webpack = require("webpack");
-const withSize = require("next-size");
 const withTypeScript = require("@zeit/next-typescript");
 
-module.exports = withSize(withTypeScript());
+module.exports = withTypeScript();
