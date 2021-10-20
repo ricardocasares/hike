a3fda94630134de216b4aa5f8c48ea09fcfc896c
Ricardo Casares <ricardocasares@gmail.com>
2021-10-20 13:01:51 +0200

    fix: issues in desc order

--- src/data/queries.ts
+++ src/data/queries.ts
@@ -9,28 +9,28 @@ export const ISSUE = `query issue($owner: String!, $repo: String!, $num: Int!) {
 }`;
 
 export const LAST_ISSUES = `query lastIssues($owner: String!, $repo: String!, $num: Int = 3) {
-    repository(owner:$owner, name:$repo) {
-        issues(last:$num, labels: ["published"]) {
-            edges {
-                node {
-                    title
-                    number
-                }
-            }
-        }
-    }
-}`;
-
-export const LAST_REPOSITORIES = `query lastRepositories {
-    viewer {
-      repositories(last: 18, privacy: PUBLIC, affiliations: OWNER, isFork: false, orderBy: {field: PUSHED_AT, direction: ASC}) {
-        nodes {
-          id
-          url
-          name
-          description
+  repository(owner:$owner, name:$repo) {
+    issues(last:$num, labels: ["published"], orderBy: { field: CREATED_AT, direction: DESC }) {
+      edges {
+        node {
+          title
+          number
         }
       }
     }
   }
+}`;
+
+export const LAST_REPOSITORIES = `query lastRepositories {
+  viewer {
+    repositories(last: 18, privacy: PUBLIC, affiliations: OWNER, isFork: false, orderBy: {field: PUSHED_AT, direction: ASC}) {
+      nodes {
+        id
+        url
+        name
+        description
+      }
+    }
+  }
+}
 `;
